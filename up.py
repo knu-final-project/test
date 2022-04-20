842192a0783fff38207b6998af0f0c1f4f29ec25
Hyerim <dkq50122@naver.com>
2022-04-20 16:23:14 +0900

    test
    Signed-off-by: Hyerim <dkq50122@naver.com>

--- up.py
+++ up.py
@@ -1,7 +1,4 @@
 print('haha')
 print('branch를 다시 한번 해봅니다:)')
-<<<<<<< HEAD
 print('branch pull??? push!!')
-=======
-print('다시다시!!!!')
->>>>>>> 3e246c9ab51a3b9a11bca34c3432653623009956
+print('Pull Reques X')
